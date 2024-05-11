e57a4aa3aa3c47a795d3be229f99483438414a15
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2024-05-11 15:33:25 +0000

    feat: ubuntu shell

---
+++ shell/ubuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# PRECAUTION before updating/upgrading Ubuntu
+sudo cat /etc/resolv.conf > resolv.conf.bu0
+sudo apt install resolvconf
+
+# INITIATION for using ubuntu
+sudo apt update
+sudo apt upgrade
+# install network tools, ping, curl, wget, nano, screen, firewall
+
+# SETUP basic development tools
+# makefile, git
+
+# SETUP GNU C Compiler
+
+# SETUP Docker & Kubernetes
+
+# SETUP Python
+
+# SETUP Golang
+
+# SETUP NodeJS
