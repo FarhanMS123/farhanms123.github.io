5b63a0c8fc07072bcaa94b9a4b70c8494d1c5f1d
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2024-07-09 10:53:51 +0700

    add nixos

---
+++ shell/nixos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# https://nix.dev/tutorials/first-steps/ad-hoc-shell-environments
+# https://search.nixos.org/
+
+nix-shell -p cowsay lolcat
+nix-shell -p nvim git npm
+nix-shell -p git neovim nodejs
+nix-shell -p python3
+
+nix-shell -p go
+
+# nix-collect-garbage
