ba1c64d109d7a44a85a32fd50265478442529f9e
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2024-07-17 10:32:59 +0700

    Update nixos.sh

--- shell/nixos.sh
+++ shell/nixos.sh
@@ -8,7 +8,9 @@ nix-shell -p nvim git npm
 nix-shell -p git neovim nodejs
 nix-shell -p python3
 
-nix-shell -p go
+nix-shell -p go graphviz
 nix-shell -p vitess
 
 # nix-collect-garbage
+
+# https://github.com/google/pprof
