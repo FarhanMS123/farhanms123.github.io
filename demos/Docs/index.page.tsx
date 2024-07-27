bd9996f04240fdd7ac30da61648a9e8a716630f6
Farhan Sabran <animationfar@gmail.com>
2024-07-27 13:27:33 +0700

    feat: add nodemon and live-server

--- demos/Docs/index.page.tsx
+++ demos/Docs/index.page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { FluentProvider, teamsDarkTheme } from "@fluentui/react-components";
+import { Button, FluentProvider, teamsDarkTheme } from "@fluentui/react-components";
 
 export default function Docs() {
     return <>
         <FluentProvider theme={teamsDarkTheme} style={{height:'100%', overflow:'auto', display: 'block'}}>
-            <div></div>
+            <Button appearance="primary">Hello.</Button>
         </FluentProvider>
     </>;
 }
