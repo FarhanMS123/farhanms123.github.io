3dbafb096ff24bccbcae7074d28b523b85e7b26f
Farhan Sabran <animationfar@gmail.com>
2024-07-27 18:35:17 +0700

    type: make config async vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { PluginOption, defineConfig, splitVendorChunkPlugin } from 'vite'
+import { type PluginOption, type UserConfig, defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { InputValue, __prepare_cbro_input, __push_rollup_input, virtualRouter } from './src/vite-virtual-file-router/files-router'
@@ -12,73 +12,75 @@ import { abs2rel, defaultExcluded, defaultIncluded, jtx_main, mmDefaultOpts, pat
 import DynamicPublicDirectory from './src/vite-multiple-assets';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    DynamicPublicDirectory(["**", "public/**"], {
-      ignore: [...defaultExcluded, "/public"],
-    }) as PluginOption,
-    virtualRouter(async ({ config, env }) => {
-      const files: InputValue[] = [];
-      const cwd = config.root!;
-
-      const cbro_input = __prepare_cbro_input(config);
-
-      const pattern = [...defaultIncluded];
-      // const pattern = ["demos/Docs/*.page.tsx"];
-      const mmOpts: fg.Options = {
-        ...mmDefaultOpts,
-        cwd,
-      };
-      const _files = await fg(pattern, mmOpts);
-      
-      for ( const script_src of _files ) {
-        const __files: InputValue[] = [];
-        if (mm.isMatch(script_src, pattern_js_ts, mmOpts))
-          __files.push(...src2page({ cwd, script_src }));
-        else if (mm.isMatch(script_src, pattern_jsx_tsx, mmOpts))
-          __files.push(...src2page({ cwd, script_src, main_out: { out: `${abs2rel(cwd, script_src)}.tsx`, raw: jtx_main } }))
-        else if (mm.isMatch(script_src, pattern_html, mmOpts))
-          __push_rollup_input(cbro_input, path.resolve(script_src))
-
-        for (const file of __files)
-          if (file.inject != "file") {
-            if (mm.isMatch(file.out, pattern_index_page_html, {...mmOpts, basename: true}))
-              file.out = `${file.out.replaceAll(/\.page\.\w+\.html$/ig, "")}.html`;
-            else if (mm.isMatch(file.out, pattern_out_html, {...mmOpts, basename: true}))
-              file.out = `${file.out.replaceAll(/\.page\.\w+\.html$/ig, "")}/index.html`;
-          }
+export default defineConfig(async ({ command, mode }) => {
+  return {
+    plugins: [
+      DynamicPublicDirectory(["**", "public/**"], {
+        ignore: [...defaultExcluded, "/public"],
+      }) as PluginOption,
+      virtualRouter(async ({ config, env }) => {
+        const files: InputValue[] = [];
+        const cwd = config.root!;
+  
+        const cbro_input = __prepare_cbro_input(config);
+  
+        const pattern = [...defaultIncluded];
+        // const pattern = ["demos/Docs/*.page.tsx"];
+        const mmOpts: fg.Options = {
+          ...mmDefaultOpts,
+          cwd,
+        };
+        const _files = await fg(pattern, mmOpts);
         
-        files.push(...__files);
-      }
-
-      return {
-        files,
-      };
-    }),
-    // showConfig,
-    tsconfigPaths({
-      loose: true,
-    }),
-    splitVendorChunkPlugin(),
-    react(),
-  ],
-
-  appType: "mpa",
-  build: {
-    outDir: "dist",
-    assetsDir: "chunks",
-  },
-  resolve: {
-    preserveSymlinks: true,
-  },
-
-  root: process.cwd(),
-  publicDir: false,
-  base: "/",
-
-  define: {
-    __TIME__: new Date().getTime(),
-    "import.meta.env.time": new Date().getTime(),
-    VITE_TIME: new Date().getTime(),
-  },
+        for ( const script_src of _files ) {
+          const __files: InputValue[] = [];
+          if (mm.isMatch(script_src, pattern_js_ts, mmOpts))
+            __files.push(...src2page({ cwd, script_src }));
+          else if (mm.isMatch(script_src, pattern_jsx_tsx, mmOpts))
+            __files.push(...src2page({ cwd, script_src, main_out: { out: `${abs2rel(cwd, script_src)}.tsx`, raw: jtx_main } }))
+          else if (mm.isMatch(script_src, pattern_html, mmOpts))
+            __push_rollup_input(cbro_input, path.resolve(script_src))
+  
+          for (const file of __files)
+            if (file.inject != "file") {
+              if (mm.isMatch(file.out, pattern_index_page_html, {...mmOpts, basename: true}))
+                file.out = `${file.out.replaceAll(/\.page\.\w+\.html$/ig, "")}.html`;
+              else if (mm.isMatch(file.out, pattern_out_html, {...mmOpts, basename: true}))
+                file.out = `${file.out.replaceAll(/\.page\.\w+\.html$/ig, "")}/index.html`;
+            }
+          
+          files.push(...__files);
+        }
+  
+        return {
+          files,
+        };
+      }),
+      // showConfig,
+      tsconfigPaths({
+        loose: true,
+      }),
+      splitVendorChunkPlugin(),
+      react(),
+    ],
+  
+    appType: "mpa",
+    build: {
+      outDir: "dist",
+      assetsDir: "chunks",
+    },
+    resolve: {
+      preserveSymlinks: true,
+    },
+  
+    root: process.cwd(),
+    publicDir: false,
+    base: "/",
+  
+    define: {
+      __TIME__: new Date().getTime(),
+      "import.meta.env.time": new Date().getTime(),
+      VITE_TIME: new Date().getTime(),
+    },
+  } as UserConfig;
 });
