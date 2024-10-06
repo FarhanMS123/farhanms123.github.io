b42412c216f1552b264e6add79d2cd9fcd397de9
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2024-10-06 02:23:27 +0000

    feat: try to use prism instead highlitghjs

--- libs/fui_docs/Markdown.tsx
+++ libs/fui_docs/Markdown.tsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, makeStyles, mergeClasses, Table, TableBody, TableCell, TableCellLayout, TableHeader, TableHeaderCell, TableRow, tokens } from "@fluentui/react-components";
 import { useQuery } from "@tanstack/react-query";
 
 import ReactMarkdown, { type Components } from 'react-markdown'
 import remarkGfm from 'remark-gfm';
-import remarkHTML from 'remark-html';
+// import remarkHTML from 'remark-html';
 import remarkYamlConfig from "remark-yaml-config";
 import remarkFrontmatter from "remark-frontmatter";
-import remarkMdx from "remark-mdx";
-import rehypeHighlight from "rehype-highlight";
-import rehypeReact from "rehype-react";
+// import remarkMdx from "remark-mdx";
+// import rehypeHighlight from "rehype-highlight";
+// import rehypeReact from "rehype-react";
 import rehypeRaw from "rehype-raw";
 
-import hljs from 'highlight.js';
-import dockerfile from "highlight.js/lib/languages/dockerfile"
-import "highlight.js/styles/an-old-hope.css"
+// import rehypeHighlight from "rehype-highlight";
+// import hljs from 'highlight.js';
+// import dockerfile from "highlight.js/lib/languages/dockerfile"
+// import "highlight.js/styles/an-old-hope.css"
+
+// import remarkPrism from "remark-prism";
+import prism from 'prismjs';
+import "prismjs/themes/prism-okaidia.min.css";
+/// @ts-ignore
+import "prismjs/plugins/line-numbers/prism-line-numbers.js";
+import "prismjs/plugins/line-numbers/prism-line-numbers.min.css";
+/// @ts-ignore
+import "prismjs/plugins/autoloader/prism-autoloader.js";
 
 import { Viewer } from "~/viewer.html.page";
 
-hljs.registerLanguage("dockerfile", dockerfile);
+// hljs.registerLanguage("dockerfile", dockerfile);
+window.Prism = prism;
+/// @ts-ignore
+window.document.Prism = prism;
+console.log(prism);
+prism.plugins.autoloader ??= {};
+prism.plugins.autoloader.languages_path = "chunks/components/";
 
 export const useMarkdownStyles = makeStyles({
   root: {
@@ -48,6 +64,9 @@ export const components: Partial<Components> = {
       <TableCellLayout>{children}</TableCellLayout>
     </TableCell>
   },
+  pre ({ node, children, ...props }) {
+    return <pre {...props} className={`${ props?.className ?? "" } line-numbers`}>{ children }</pre>
+  }
 };
 
 export function MarkdownURL({ url, className }: Viewer) {
@@ -74,15 +93,27 @@ ${"```"}
 export function Markdown({ className, content }: Omit<Viewer, "url">) {
   const styles = useMarkdownStyles();
 
+  /// @ts-ignore
+  Prism?.highlightAll();
+
   return <div className={mergeClasses(styles.root, "p-2 lg:p-4", className)}>
     <ReactMarkdown remarkPlugins={[
       remarkFrontmatter,
       remarkYamlConfig,
       // remarkHTML,
+      /// @ts-ignore
+      // [remarkPrism, {
+      //   plugins: [
+      //     'line-numbers',
+      //     'autoloader',
+      //     // prism_lineNumbers,
+      //     // prism_autoloader,
+      //   ],
+      // }],
       remarkGfm,
       // remarkMdx,
     ]} rehypePlugins={[
-      [rehypeHighlight, {detect: true}],
+      // [rehypeHighlight, {detect: true}],
       // rehypeReact,
       rehypeRaw,
     ]} components={components} skipHtml={false}>{content}</ReactMarkdown>
